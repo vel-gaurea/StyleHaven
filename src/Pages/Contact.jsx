a5bb90e8fe2d8ea8cb8b10ada7e10db86e00517e
vel-gaurea <velsongaurea@gmail.com>
2024-09-20 08:24:34 +0530

    Style Haven Logo modification

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -14,9 +14,9 @@ const Contact = () => {
         <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="" />
         <div className='flex flex-col justify-center items-start gap-6'>
           <p className='font-semibold text-xl text-gray-600'>Our Store</p>
-          <p className='text-gray-500'>Forever Shop<br />1234 Elm Street, Apt 56<br />Springfield, IL 62704<br />United States</p>
-          <p className='text-gray-500'>Phone: (555) 123-4567 <br />Email: support@forever.com</p>
-          <p className='font-semibold text-xl text-gray-600'>Careers at Forever</p>
+          <p className='text-gray-500'>StyleHaven Shop<br />1234 Elm Street, Apt 56<br />Springfield, IL 62704<br />United States</p>
+          <p className='text-gray-500'>Phone: (555) 123-4567 <br />Email: support@stylehaven.com</p>
+          <p className='font-semibold text-xl text-gray-600'>Careers at StyleHaven</p>
           <p className='text-gray-500'>Learn more about our teams and job opening</p>
           <button className='border border-black rounded-full px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500 '>Explore Jobs</button>
         </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -25,14 +25,14 @@ const Footer = () => {
                     <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
                     <ul className='flex flex-col gap-1 text-gray-600'>
                         <li>+1-212-456-7890</li>
-                        <li>contact@foreveryou.com</li>
+                        <li>contact@stylehaven.com</li>
                         
                     </ul>
                 </div>
 
             </div>
                 <hr />
-                <p className='py-5 text-sm text-center'>Copyright 2024@ forever.com - All Rights Reserved.</p>
+                <p className='py-5 text-sm text-center'>Copyright 2024@ stylehaven.com - All Rights Reserved.</p>
             <div>
 
             </div>
